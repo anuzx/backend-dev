29ba1c5d9997e67a5468d8414f1f80b4868278bb
Anuj <anujparasar4266@gmail.com>
2025-09-25 04:29:10 +0530

    Update auth.js

--- url_shortener_statefull/service/auth.js
+++ url_shortener_statefull/service/auth.js
@@ -1,11 +1,12 @@
 const sessionIdToUserMap = new Map();
 
 function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+  sessionIdToUserMap.set(id, user); //The set() method is used to add a new key-value pair 
+                                  //to a Map object or to update the value associated with an existing key.
 }
 
 function getUser(id) {
-  return sessionIdToUserMap.get(id);
+  return sessionIdToUserMap.get(id); //get() method to retrieve the value associated with a specific key in the map.
 }
 
 module.exports = {
