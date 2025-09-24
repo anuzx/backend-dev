c77046e8d4a535a3a8f9b0423fd487a97a34987e
Anuj <anujparasar4266@gmail.com>
2025-09-24 20:07:19 +0530

    Update url.js

--- url_shortener_statefull/controllers/url.js
+++ url_shortener_statefull/controllers/url.js
@@ -1,14 +1,16 @@
 const { nanoid } = require("nanoid");
-const URL = require("../models/url");
+const URL = require("../models/url"); //importing database , as we want to store our shortID in the database
 
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
   const shortID = nanoid(8);
 
-  await URL.create({
+  await URL.create({ //creates a new url in database
+
+    //properties of the url in database
     shortId: shortID,
-    redirectURL: body.url,
+    redirectURL: body.url, //url given by user 
     visitHistory: [],
     createdBy: req.user._id,
   });
