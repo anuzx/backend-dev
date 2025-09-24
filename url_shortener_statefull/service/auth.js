14d34a5cf80f07a26df1bd36b03b8e6d2a1acb68
Anuj <anujparasar4266@gmail.com>
2025-09-25 05:27:22 +0530

    Update auth.js

--- url_shortener_statefull/service/auth.js
+++ url_shortener_statefull/service/auth.js
@@ -1,5 +1,9 @@
 const sessionIdToUserMap = new Map();
 
+//this lives inside our Node.js process memory.When you restart the server, the process stops
+//RAM is wiped clean ,he cookie in your browser isn’t erased; 
+//it just points to a session ID that no longer exists on the server. 
+
 function setUser(id, user) {
   sessionIdToUserMap.set(id, user); //The set() method is used to add a new key-value pair 
                                   //to a Map object or to update the value associated with an existing key.
